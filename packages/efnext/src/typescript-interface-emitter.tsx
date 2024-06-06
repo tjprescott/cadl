3c11717371d16d135dcc453d8f35b0436a07d194
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2024-06-05 22:00:36 -0700

    Organize files by namespace

--- packages/efnext/src/typescript-interface-emitter.tsx
+++ packages/efnext/src/typescript-interface-emitter.tsx
@@ -1,4 +1,5 @@
 import { EmitContext, Program } from "@typespec/compiler";
+import { dirname, join } from "path";
 import { format } from "prettier";
 import { EmitOutput, SourceFile } from "../src/framework/components/index.js";
 import { EnumDeclaration } from "../src/typescript/enum-declaration.js";
@@ -6,13 +7,16 @@ import { Function } from "../src/typescript/function.js";
 import { InterfaceDeclaration } from "../src/typescript/interface-declaration.js";
 import { ScalarDeclaration } from "../src/typescript/scalar-declaration.js";
 import { UnionDeclaration } from "../src/typescript/union-declaration.js";
+import { SourceDirectory } from "./framework/components/source-directory.js";
 import { renderToSourceFiles } from "./framework/core/render.js";
 
 export async function $onEmit(context: EmitContext) {
   const rendered = await renderToSourceFiles(emitTypescriptInterfaces(context.program));
   for (const file of rendered) {
+    const path = join(context.emitterOutputDir, file.path);
+    await context.program.host.mkdirp(dirname(path));
     const output = await format(file.content, { parser: "typescript" });
-    await context.program.host.writeFile(file.path, output);
+    await context.program.host.writeFile(path, output);
   }
 }
 
@@ -22,44 +26,48 @@ export function emitTypescriptInterfaces(program: Program) {
   const namespaces = [...globalNamespace.namespaces.values()].filter(
     (ns) => ns.name !== "TypeSpec"
   );
-  const models = [...globalNamespace.models.values()];
-  const operations = [...globalNamespace.operations.values()];
-  const enums = [...globalNamespace.enums.values()];
-  const unions = [...globalNamespace.unions.values()];
-  const interfaces = [...globalNamespace.interfaces.values()];
-  const scalars = [...globalNamespace.scalars.values()];
 
-  for (const namespace of namespaces) {
-    models.push(...namespace.models.values());
-    operations.push(...namespace.operations.values());
-    enums.push(...namespace.enums.values());
-    unions.push(...namespace.unions.values());
-    interfaces.push(...namespace.interfaces.values());
-    scalars.push(...namespace.scalars.values());
-  }
+  namespaces.unshift(globalNamespace);
 
   return (
     <EmitOutput>
-      <SourceFile path="test1.ts" filetype="typescript">
-        {models.map((model) => (
-          <InterfaceDeclaration type={model} />
-        ))}
-        {operations.map((operation) => (
-          <Function type={operation} />
-        ))}
-        {enums.map((enumType) => (
-          <EnumDeclaration type={enumType} />
-        ))}
-        {unions.map((union) => (
-          <UnionDeclaration type={union}></UnionDeclaration>
-        ))}
-        {interfaces.map((iface) => (
-          <InterfaceDeclaration type={iface} />
-        ))}
-        {scalars.map((scalar) => (
-          <ScalarDeclaration type={scalar} />
-        ))}
-      </SourceFile>
+      {namespaces.map((namespace) => {
+        const models = [...namespace.models.values()];
+        const operations = [...namespace.operations.values()];
+        const enums = [...namespace.enums.values()];
+        const unions = [...namespace.unions.values()];
+        const interfaces = [...namespace.interfaces.values()];
+        const scalars = [...namespace.scalars.values()];
+
+        const file = (
+          <SourceFile path="index.ts" filetype="typescript">
+            {models.map((model) => (
+              <InterfaceDeclaration type={model} />
+            ))}
+            {operations.map((operation) => (
+              <Function type={operation} />
+            ))}
+            {enums.map((enumType) => (
+              <EnumDeclaration type={enumType} />
+            ))}
+            {unions.map((union) => (
+              <UnionDeclaration type={union}></UnionDeclaration>
+            ))}
+            {interfaces.map((iface) => (
+              <InterfaceDeclaration type={iface} />
+            ))}
+            {scalars.map((scalar) => (
+              <ScalarDeclaration type={scalar} />
+            ))}
+          </SourceFile>
+        );
+
+        if (namespace.name) {
+          return <SourceDirectory path={namespace.name}>{file}</SourceDirectory>;
+        }
+
+        return file;
+      })}
     </EmitOutput>
   );
 }
